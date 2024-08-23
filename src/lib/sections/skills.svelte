7e07154eb0279704206d40ddcff84cf074271efe
Richard Tammar <venusian_cat@hotmail.com>
2024-08-23 11:10:52 -0700

    Refactored skillset headings as strings

--- src/lib/sections/skills.svelte
+++ src/lib/sections/skills.svelte
@@ -4,34 +4,39 @@
 
 	const skillSets = [
 		{
-			skillSetHeading: ['Getting Things Done'],
+			skillSetHeading: 'Getting Things Done',
 			icon: 'fa-check-square',
-			description: 'I\'m deeply pragmatic and have a strong bias for action.'
+			description: "I'm deeply pragmatic and have a strong bias for action."
 		},
 		{
-			skillSetHeading: ['Bridging Domains'],
+			skillSetHeading: 'Bridging Domains',
 			icon: 'fa-magic',
-			description: 'I speak executive and engineer and I enjoy the challenge of matching praxis to strategic objectives.'
+			description:
+				'I speak executive and engineer and I enjoy the challenge of matching praxis to strategic objectives.'
 		},
 		{
-			skillSetHeading: ['Building Teams'],
+			skillSetHeading: 'Building Teams',
 			icon: 'fa-code-branch',
-			description: 'I nurture the creativity that emerges from the diversity of our unique backgrounds and experiences.'
+			description:
+				'I nurture the creativity that emerges from the diversity of our unique backgrounds and experiences.'
 		},
 		{
-			skillSetHeading: ['Conveying Ideas'],
+			skillSetHeading: 'Conveying Ideas',
 			icon: 'fa-lightbulb',
-			description: '"Clear, smart, digestible... and highly entertaining"... professional teacher training has ensured zero Powerpoint related deaths since 1998.'
+			description:
+				'"Clear, smart, digestible... and highly entertaining"... professional teacher training has ensured zero Powerpoint related deaths since 1998.'
 		},
 		{
-			skillSetHeading: ['Designing Systems'],
+			skillSetHeading: 'Designing Systems',
 			icon: 'fa-cogs',
-			description: 'From information architecture to process engineering, I\'m deeply fascinated by systems of all stripes.'
+			description:
+				"From information architecture to process engineering, I'm deeply fascinated by systems of all stripes."
 		},
 		{
-			skillSetHeading: ['Coding'],
+			skillSetHeading: 'Coding',
 			icon: 'fa-code',
-			description: 'Full-stack if not full-time, I know more than enough to be dangerous.'
+			description:
+				'Full-stack if not full-time, I know more than enough to be dangerous.'
 		}
 	];
 
@@ -62,14 +67,13 @@
 		<div class="grid gap-8 md:grid-cols-2 lg:grid-cols-3">
 			{#each skillSets as o, i}
 				<Window icon={o.icon}>
-					{#each o.skillSetHeading as h, j}
-						<EditorLine class="font-bold text-green-700">
-							// &nbsp;<!--font awesome icon-->
-							<i class="fas {o.icon} fa-inverse text-green-700"></i>
-							&nbsp;
-							<!--end font awesome icon-->{h}
-						</EditorLine>
-					{/each}
+					<EditorLine class="font-bold text-green-700">
+						// &nbsp;<!--font awesome icon-->
+						<i class="fas {o.icon} fa-inverse text-green-700"></i>
+						&nbsp;
+						<!--end font awesome icon-->{o.skillSetHeading}
+					</EditorLine>
+
 					<EditorLine class="font-normal text-zinc-300">/*</EditorLine>
 
 					{#each splitStringWithoutBreakingWords(o.description, 35) as s, j}
