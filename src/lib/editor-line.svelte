3eb16a2f28512fca42a04dc187675b9c0bbddece
Richard Tammar <venusian_cat@hotmail.com>
2025-01-09 17:01:17 -0800

    Initial swap out of Inter for Lato

--- src/lib/editor-line.svelte
+++ src/lib/editor-line.svelte
@@ -3,7 +3,7 @@
 
 <code
 	class={$$restProps.class +
-		" relative block overflow-hidden text-ellipsis whitespace-nowrap break-words font-['Inter'] before:mr-3 before:inline-block before:w-5 before:border-r-2 before:border-r-indigo-200/80 before:pr-1 before:text-right before:font-['Roboto_Mono'] before:text-sm before:text-zinc-300"}>
+		" relative block overflow-hidden text-ellipsis whitespace-nowrap break-words font-['Lato'] before:mr-3 before:inline-block before:w-5 before:border-r-2 before:border-r-indigo-200/80 before:pr-1 before:text-right before:font-['Roboto_Mono'] before:text-sm before:text-zinc-300"}>
 	<slot />
 </code>
 
