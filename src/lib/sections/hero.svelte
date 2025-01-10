7a71c2d1168c2ef87c354f5be7510aac27336357
Richard Tammar <venusian_cat@hotmail.com>
2025-01-10 12:19:58 -0800

    Minor cosmetic change to banner

--- src/lib/sections/hero.svelte
+++ src/lib/sections/hero.svelte
@@ -8,14 +8,14 @@
 	<!--author block, non-mobile-->
 	<div class="hidden px-4 py-6 text-5xl sm:block">
 		<p class="font-['Roboto_Mono'] tracking-wide text-slate-100/90">
-			<span class="text-slate-400/30">&gt;</span>Richard Tammar<span
+			<span class="text-slate-400/20">&gt;</span>Richard Tammar<span
 				class="text-slate-400">_</span>
 		</p>
 	</div>
 	<!--author block, mobile-->
 	<h1
-		class="bg-sky-100 px-4 pt-2 font-['Roboto_Mono'] text-2xl tracking-wide text-slate-900 sm:hidden">
-		<span class="text-sky-300">&gt;</span>Richard Tammar<span
+		class="bg-sky-100 px-2 pt-2 font-['Roboto_Mono'] text-2xl tracking-wide text-slate-900 sm:hidden">
+		<span class="text-sky-300">&gt;&nbsp;</span>Richard Tammar<span
 			class="text-sky-300">_</span>
 	</h1>
 
@@ -31,11 +31,11 @@
 	</div>
 
 	<p
-		class="bg-slate-500 pb-8 pl-12 pr-3 pt-2 font-['Roboto_Mono'] leading-6 text-slate-100/90 sm:max-w-[30rem] sm:bg-transparent sm:text-xl">
+		class="bg-slate-500 pb-8 pl-4 sm:pl-12 pr-3 pt-2 font-['Roboto_Mono'] text-slate-100/90 sm:max-w-[30rem] sm:bg-transparent sm:text-xl md:max-w-[42rem]">
 		<img
 			src="{base}/images/headshot-square.jpg"
 			alt="headshot of Richard Tammar"
-			class="visible float-right mr-2 size-24 rounded-full sm:hidden" />
+			class="visible float-right ml-1 mb-1 mr-2 size-24 rounded-full sm:hidden" />
 		Veteran technologist with a knack for connecting the C-suite to the engine room.
 		Empathetic, curious and analytical, I work to catalyze positive change.
 	</p>
